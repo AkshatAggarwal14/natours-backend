3593c58c7ef5583ab9e804b85d9b8207acfaa1b6
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2023-06-23 12:45:27 +0530

    Used Param Middleware and Chained Middlewares

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,10 +3,16 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
+//! In a param middleware we get access to the param through val argument
+// We can use this middleware to check if ID is valid!
+// This middleware is a part of our pipeline now! => Validation of ID is done automatically by this param middleware
+router.param('id', tourController.checkID);
+
+// we can also chain middlewares when passing handlers!
 router
     .route('/')
     .get(tourController.getAllTours)
-    .post(tourController.createTour);
+    .post(tourController.checkBody, tourController.createTour);
 
 router
     .route('/:id')
