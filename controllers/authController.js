f8f90fca2393def2a0b50d847378dede698b645f
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2023-07-02 00:32:55 +0530

    Added signup and login

---
+++ controllers/authController.js
@@ -0,0 +1,56 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const signToken = (id) =>
+    jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+
+exports.signup = catchAsync(async (req, res, next) => {
+    //! Wrong as anyone can POST role: 'admin' to attain admin privileges
+    // const newUser = await User.create(req.body);
+
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm,
+    });
+
+    //! To generate secret
+    // node -e "console.log(require('crypto').randomBytes(64).toString('hex'));"
+    const token = signToken(newUser._id);
+
+    res.status(201).json({
+        status: 'success',
+        token: token,
+        data: {
+            //! this will still display 'password' even though select: false
+            user: newUser,
+        },
+    });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return next(new AppError('Please provide email and password', 400));
+    }
+
+    const user = await User.findOne({ email }).select('+password');
+    // this document has access to email, password
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+        return next(new AppError('Incorrect email or password', 401));
+    }
+
+    const token = signToken(user._id);
+
+    res.status(200).json({
+        status: 'success',
+        // now we are logged in using this token!
+        token,
+    });
+});
---
+++ models/userModel.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please tell us your name'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please provide your email'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email'],
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: ['Please provide a password'],
+        minLength: 8,
+        select: false,
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password'],
+        validate: {
+            // This only works on CREATE and SAVE!
+            validator: function (el) {
+                return el === this.password;
+            },
+            message: 'Password does not match Confirm Password',
+        },
+    },
+});
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    this.password = await bcrypt.hash(this.password, 12);
+
+    this.passwordConfirm = undefined;
+    next();
+});
+
+// here this is document!
+// this.password is not available as it is select: false
+//! Schema Instance method
+userSchema.methods.correctPassword = async function (
+    candidatePassword,
+    userPassword
+) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
