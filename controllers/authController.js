e4d67589628c52773e0cfae1f8d3c0d5055894ba
Akshat Aggarwal <akshataggarwal1411@gmail.com>
2023-07-02 01:43:02 +0530

    Protected Get all tour Route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
@@ -53,4 +54,61 @@ exports.login = catchAsync(async (req, res, next) => {
         // now we are logged in using this token!
         token,
     });
+});
+
+// routes are protected using .protect middleware
+exports.protect = catchAsync(async (req, res, next) => {
+    // Getting token and check if its there
+
+    let token;
+    // usually in headers as 'Authorization:Bearer <token>'
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        token = req.headers.authorization.split(' ')[1];
+    }
+
+    if (!token) {
+        return next(
+            new AppError(
+                'You are not logged in! Please log in to get access',
+                401
+            )
+        );
+    }
+
+    // Verify token and handle 'TokenExpiredError' & 'JsonWebTokenError'
+    const decodedPayload = await promisify(jwt.verify)(
+        token,
+        process.env.JWT_SECRET
+    );
+    // jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    //     console.log(decoded.id);
+    // });
+
+    // Check if user still exists
+    const currentUser = await User.findById(decodedPayload.id);
+    if (!currentUser) {
+        return next(
+            new AppError(
+                'The user belonging to this token no longer exists!',
+                401
+            )
+        );
+    }
+
+    // Check if user changed password after token was issued
+    if (currentUser.changedPasswordAfter(decodedPayload.iat)) {
+        return next(
+            new AppError(
+                'User recently changed password! Please log in again.',
+                401
+            )
+        );
+    }
+
+    // GRANT ACCESS TO PROTECTED ROUTE
+    req.user = currentUser;
+    next();
 });
